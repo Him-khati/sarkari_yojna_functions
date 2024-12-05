8abdc93333e5ecfd1d3d405fdde2203dc9a4dda4
Himanshu Khati <him.khati@gmail.com>
2024-12-05 16:01:56 +0530

    Code Fix

---
+++ functions/yojna-operations-function/src/factory/request_handler_factory.js
@@ -0,0 +1,14 @@
+import { YojnaRequestHandler } from "../request_handlers/yojna_request_handler";
+
+export function createYojnaRequestHandler(
+   authorRepository,
+   yojnaRepository,
+   tagRepository
+) {
+
+   return YojnaRequestHandler(
+      authorRepository = authorRepository,
+      tagRepository = tagRepository,
+      yojnaRepository =  yojnaRepository
+   );
+}
